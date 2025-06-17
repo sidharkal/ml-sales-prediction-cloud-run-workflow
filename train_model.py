4740e6c59dd1403c297cda85a01c82270e28cfe2
sidheshwar <sidheshwar.harkal@fractal.ai>
2025-06-17 07:36:42 +0000

    added code into repo

---
+++ train_model.py
@@ -0,0 +1,15 @@
+import pickle 
+from sklearn.linear_model import LinearRegression
+import numpy as np
+
+X = np.array([[100],[200],[300],[400],[500]])
+y =  np.array([10,20,30,40,50])
+
+model = LinearRegression()
+
+model.fit(X,y)
+
+# Save the Model
+
+with open("model.pkl","wb") as f:
+    pickle.dump(model,f)
