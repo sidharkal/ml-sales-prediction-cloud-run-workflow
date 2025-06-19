0a38005de8f1880e22b91d9e29e2b972b8f26e6b
sidheshwar <sidheshwar.harkal@fractal.ai>
2025-06-19 13:44:02 +0000

    updated cloudbuid and other files

--- app.py
+++ app.py
@@ -1,24 +1,27 @@
-from flask import Flask,request,jsonify
+from flask import Flask, request, jsonify
 import pickle
 import numpy as np
+import os
 
 app = Flask(__name__)
 
 # Load the model
-with open('model.pkl','rb') as f:
-   model = pickle.load(f)
+with open('model.pkl', 'rb') as f:
+    model = pickle.load(f)
 
 @app.route("/")
 def home():
-   return jsonify({"message":"Sales Prediction API is Running"})
+    return jsonify({"message": "Sales Prediction API is Running"})
 
-@app.route('/predict',methods=['POST'])
+@app.route('/predict', methods=['POST'])
 def predict():
-   data = request.get_json()
-   features = np.array(data['features']).reshape(1,-1)
-   prediction = model.predict(features)
-   return jsonify({'predicted_sales': prediction[0]})
+    data = request.get_json()
+    features = np.array(data['features']).reshape(1, -1)
+    prediction = model.predict(features)
+    return jsonify({'predicted_sales': prediction[0]})
 
 if __name__ == '__main__':
-   app.run(host='0.0.0.0',port=8080)
+    port = int(os.environ.get('PORT', 8080))  # Use PORT env var if set, else 8080
+    app.run(host='0.0.0.0', port=port)
+
 
