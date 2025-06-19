fa43509c1a8c0ab2a728f722496315d19c7c91f5
sidheshwar <sidheshwar.harkal@fractal.ai>
2025-06-19 13:52:36 +0000

    updated cloudbuid and other files

--- app.py
+++ app.py
@@ -22,6 +22,7 @@ def predict():
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 8080))  # Use PORT env var if set, else 8080
+    print(f"Starting Flask app on port {port}...")
     app.run(host='0.0.0.0', port=port)
 
 
